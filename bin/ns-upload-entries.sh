aec52822bbac7e508f521a85a3ffdae62aaed87b
Scott Leibrand <scottleibrand@gmail.com>
2015-10-24 00:02:36 -0700

    revert to contents of old ns-upload.sh until wip/bewest/dev is merged

--- bin/ns-upload-entries.sh
+++ bin/ns-upload-entries.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Author: Ben West
-# Maintainer: @cjo20, @scottleibrand
+# Author: Ben West, Maintainer: Scott Leibrand
 
-ENTRIES=${1-entries.json}
-NIGHTSCOUT_HOST=${NIGHTSCOUT_HOST-localhost:1337}
+HISTORY=${1-pumphistory.json}
+OUTPUT=${2-pumphistory.ns.json}
 #TZ=${3-$(date +%z)}
-OUTPUT=${2}
 
-export ENTRIES API_SECRET NIGHTSCOUT_HOST
+cat $HISTORY | \
+  json -e "this.medtronic = this._type;" | \
+  #json -e "this.dateString = this.timestamp + '$(TZ=TZ date +%z)'" | \
+  json -e "this.dateString = this.timestamp + '$(date +%z)'" | \
+  json -e "this.type = 'medtronic'" | \
+  json -e "this.date = this.date ? this.date : new Date(Date.parse(this.dateString)).getTime( )" \
+  > $OUTPUT
+
+
 # requires API_SECRET and NIGHTSCOUT_HOST to be set in calling environment (i.e. in crontab)
-(
-curl -s -X POST --data-binary @$ENTRIES \
-  -H "API-SECRET: $API_SECRET" \
-  -H "content-type: application/json" \
-  $NIGHTSCOUT_HOST/api/v1/entries.json
-) && ( test -n "$OUTPUT" && touch $OUTPUT ; logger "Uploaded $ENTRIES to $NIGHTSCOUT_HOST" ) || logger "Unable to upload to $NIGHTSCOUT_HOST"
-
+curl -s -X POST --data-binary @$OUTPUT -H "API-SECRET: $API_SECRET" -H "content-type: application/json" $NIGHTSCOUT_HOST/api/v1/entries.json >/dev/null && ( touch /tmp/openaps.online && echo "Uploaded $OUTPUT to $NIGHTSCOUT_HOST" ) || echo "Unable to upload to $NIGHTSCOUT_HOST"
